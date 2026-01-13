ab29bf68b55c9b411b2e453eefd9c17ed4f2a909
kncoo1 <nickelkj11@gmail.com>
2026-01-13 10:10:26 -0600

    Create page.tsx

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Login</h1>
+      <input placeholder="Email" />
+      <input placeholder="Password" type="password" />
+      <button>Login</button>
+    </main>
+  );
+}
